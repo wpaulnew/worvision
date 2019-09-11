e98c89bf620b60a00381b0c431742b3095475dc3
wpaulnew <wpaulnew@gmail.com>
2019-09-11 12:26:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Worvision
+
+
+```
+# Clone this repository
+git clone https://github.com/wpaulnew/worvision
+# Go into the repository
+cd worvision
+# Install dependencies
+yarn install
+# Run the app
+yarn run dev
+```
---
+++ main.js
@@ -0,0 +1,89 @@
+// Modules to control application life and create native browser window
+const electron = require('electron');
+const {app, BrowserWindow, ipcMain, WebContents} = require('electron');
+const path = require('path');
+
+// Use express
+const express = require('express');
+
+// Enable live reload for all the files inside your project directory
+require('electron-reload')(__dirname);
+
+let mainWindow;
+let remoteWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.maximize();
+
+  mainWindow.loadFile('./app/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', () => {
+
+  // Connect to api
+  const api = express();
+  const port = 3000;
+  api.get('/', (req, res) => {
+    // const text = localStorage.getItem('text');
+    // res.send(text);
+  });
+  api.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+  // Create windows
+  createWindow();
+
+  // Create remote window
+  let displays = electron.screen.getAllDisplays();
+  let externalDisplay = displays.find((display) => {
+    return display.bounds.x !== 0 || display.bounds.y !== 0
+  });
+
+  if (externalDisplay) {
+    remoteWindow = new BrowserWindow({
+      x: externalDisplay.bounds.x + 50,
+      y: externalDisplay.bounds.y + 50,
+      fullscreen: true,
+      webPreferences: {
+        nodeIntegration: true
+      }
+    });
+
+    remoteWindow.maximize();
+
+    // and load the index.html of the app.
+    remoteWindow.loadFile('./app/remote.html');
+  }
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+});
+
+ipcMain.on('close-main-window', function () {
+  app.quit();
+});
+
+ipcMain.on('show-remote-screen', function () {
+
+});
+
+ipcMain.on('change-text', (event, args) => {
+  remoteWindow.webContents.send('get-new-text', args);
+});
